e68eaa6d7f2b313016c841dd25fdbc1eb2c932f0
Manan Aggarwal <managgar3@publicisgroupe.net>
2023-05-24 17:50:17 +0530

    updating route6

--- src/app/route6/route6.component.ts
+++ src/app/route6/route6.component.ts
@@ -3,24 +3,23 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-route6',
   templateUrl: './route6.component.html',
-  styleUrls: ['./route6.component.css'] 
+  styleUrls: ['./route6.component.css']
 })
 export class Route6Component {
 
   arrSize = 5;
- 
-  scrollBox(event: any){
-    // console.log(event.getBoundingClientRect().height);
-    const target: any = document.getElementById('targetDiv');
-    // console.log(target.getBoundingClientRect().top);
-    if(target.getBoundingClientRect().top < event.getBoundingClientRect().height){
+  @ViewChild('targetDiv') target!: ElementRef;
+
+  scrollBox(event: any) {
+    const divEl = this.target.nativeElement;
+    if (divEl.getBoundingClientRect().top < event.getBoundingClientRect().height) {
       this.arrSize += 5;
     }
-   
-    
+
+
   }
-  showAlert(event: any){
-    alert("Button of "+event.target.value+"th div is clicked!");
+  showAlert(event: any) {
+    alert("Button of " + event.target.value + "th div is clicked!");
   }
 
 }
