cb7c0c02e717aef8d7b55856fc6b58a351ae7b57
Manan Aggarwal <managgar3@publicisgroupe.net>
2023-05-24 13:32:20 +0530

    adding route6

--- src/app/route6/route6.component.ts
+++ src/app/route6/route6.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-route6',
   templateUrl: './route6.component.html',
-  styleUrls: ['./route6.component.css']
+  styleUrls: ['./route6.component.css'] 
 })
 export class Route6Component {
+
+  arrSize = 5;
+ 
+  scrollBox(event: any){
+    // console.log(event.getBoundingClientRect().height);
+    const target: any = document.getElementById('targetDiv');
+    // console.log(target.getBoundingClientRect().top);
+    if(target.getBoundingClientRect().top < event.getBoundingClientRect().height){
+      this.arrSize += 5;
+    }
+   
+    
+  }
+  showAlert(event: any){
+    alert("Button of "+event.target.value+"th div is clicked!");
+  }
 
 }
--- src/app/route6/route6.module.ts
+++ src/app/route6/route6.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { Route6RoutingModule } from './route6-routing.module';
 import { Route6Component } from './route6.component';
 import { SharedModule } from '../shared.module';
+import { Range } from './range.pipe';
 
 
 @NgModule({
   declarations: [
-    Route6Component
+    Route6Component,
+    Range
   ],
   imports: [
     SharedModule,
